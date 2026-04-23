98107c8bc8f437f5ddd5529286ec1945e5b9710b
ignaciogalmes <ignaciogalmes79@gmail.com>
2026-04-23 13:59:11 -0300

    login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-iimport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import Inventario from './Inventario'; 
 
